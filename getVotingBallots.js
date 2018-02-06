74a0764969258d485a48b544a9e5a0ef33400fce
Johnny Nuuma <jnuuma@gmail.com>
2018-02-06 10:38:59 -0800

    getVotingBallots now publishes new ballots to mq.db

--- getVotingBallots.js
+++ getVotingBallots.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var log4js = require('log4js');
+var Queue = require('./mq.js') ;
+var q = new Queue('./mq.db') ;
 log4js.configure({
     appenders: { voting: { type: 'file', filename: 'votingBallots.log' } },
     categories: { default: { appenders: ['voting'], level: 'debug' } }
@@ -8,6 +10,7 @@ var logger = log4js.getLogger();
 var block = fs.readFileSync('block', 'utf-8');
 var endBlock = block;
 var memo = "yada";
+
 
 const POA_ABI = require('./voting.json');
 const Web3 = require('web3');
@@ -20,39 +23,51 @@ const poa = new web3.eth.Contract(POA_ABI, '0xc40cdf254a4a35498aa84f35e9842c1107
 logger.debug("\n -------------------------\n");
 logger.debug("from block: " + block );
 
-/*
-function getMemo(id) {
-    logger.debug("id: " + id );
-     poa.methods.getMemo(id).call().then( function(response) { memo = response.data; } );
-     return memo;
-}
-*/
+q.on('open',function() {
+    console.log('Opening SQLite DB') ;
+    console.log('Queue contains '+q.getLength()+' job/s') ;
+}) ;
+ 
+q.on('add',function(task) {
+    console.log('Adding task: '+JSON.stringify(task)) ;
+    console.log('Queue contains '+q.getLength()+' job/s') ;
+}) ;
+
+//open connection to our queuec
+q.open( )
+.then(function() {
+})
+.catch(function(err) {
+    console.log('Error occurred:') ;
+    console.log(err) ;
+    process.exit(1) ;
+});
 
 poa.getPastEvents('BallotCreated',{
     fromBlock: block 
-}, function(error, events){ events.forEach((e) => { 
-      logger.debug(e.event,'(', e.blockNumber, ') -- ', e  ); 
-      endBlock = e.blockNumber; 
-      logger.debug('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-      var p1 = poa.methods.votingState(e.returnValues.id).call().then( 
-          resp =>  logger.debug("ballotId[" + e.returnValues.id + "]: " + JSON.stringify( resp ) )
-       );    
-    } )  } )
+}, function(error, events){ 
+    if ( events.length > 0 ) {}
+    logger.debug("Found [" + events.length + "] new ballots.");
+    if ( events.length > 0 ) {
+        events.forEach( ( e ) => { 
+            //logger.debug(e.event,'(', e.blockNumber, ') -- ', e  ); 
+            endBlock = e.blockNumber; 
+            //logger.debug('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+            var p1 = poa.methods.votingState(e.returnValues.id).call().then( 
+                //resp =>  q.add( JSON.stringify(resp) )
+                resp => logger.debug("ballotId[" + e.returnValues.id + "]: " + resp.memo )
+             );    
+          } ) 
+    } 
+} )
 .then(function(events){
- //   console.log(events) // same results as the optional callback above
  if ( block != endBlock ) endBlock++;
 // Uncomment the following to persistently store lastest block to disk.
-/* 
- s.writeFile("block", endBlock , function(err)
+
+ fs.writeFile("block", endBlock , function(err)
  {
     if ( err ) { return console.log(err); } 
-     //console.log("start block is now: " + ( endBlock) );  } );
-*/ 
+     //console.log("start block is now: " + ( endBlock) );  
+ } );
 console.log("done");
 });
-
-
-
-
-
-
